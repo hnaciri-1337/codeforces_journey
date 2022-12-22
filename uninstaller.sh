05baa6b113f9e230e0ef569a80bb153a44b74ed3
Haitham Naciri ben saghir <hnaciri-@e1r2p8.1337.ma>
2022-12-22 11:07:22 +0100

    update uninstaller

--- uninstaller.sh
+++ uninstaller.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -rf ~/.codeforces_journey ~/.codeforces_journey.sh
+grep -v "source ~/.codeforces_journey.sh" ~/.zshrc > .temp && mv .temp ~/.zshrc
 
 echo -e	"	   	   \033[33m command uninstalled successfully !\n"
-echo -e	"	   	   \033[00m open ~/.zshrc and delete this \"line source ~/.codeforces_journey.sh\" \n"
